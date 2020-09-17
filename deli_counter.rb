b339362af5957734148c9ef6bf4fb2c33e087560
Erick Castille <ecastille924@gmail.com>
2020-09-17 23:11:55 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -4,7 +4,7 @@ def line(names)
   if names.empty?
     puts "The line is currently empty."
   else
-    positions = names.map.with_index(1) {|name, index| "#{index}. #{name}"
+    positions = names.map.with_index(1) {|name, index| "#{index}. #{name}"}
     stringed_positions = positions.join(" ")
     puts "The line is currently: " + stringed_positions
   end

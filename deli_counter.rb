84c2e197d1b0b21f6555ddc8b8744a09eb375c55
Erick Castille <ecastille924@gmail.com>
2020-09-17 23:11:06 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -4,7 +4,7 @@ def line(names)
   if names.empty?
     puts "The line is currently empty."
   else
-    positions = names.map.with_index(1) {|name, index|"#{index}. #{name}"
+    positions = names.map.with_index(1) {|name, index| "#{index}. #{name}"
     stringed_positions = positions.join(" ")
     puts "The line is currently: " + stringed_positions
   end
@@ -12,12 +12,12 @@ end
 
 def take_a_number(names, name_to_add)
   names.push(name_to_add)
-  puts "Welcome, #{name_to_add}. Youa re number #{names.length} in line."
+  puts "Welcome, #{name_to_add}. You are number #{names.length} in line."
 end 
 
 def now_serving(names)
   if names.empty?
-    puts "There is nobody waiting to e served!"
+    puts "There is nobody waiting to be served!"
   else
     puts "Currently serving #{names.first}."
     names.shift
